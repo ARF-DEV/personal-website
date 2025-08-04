e5a1b6b3e1efcbfd830dd97407c9908a2c9d7781
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2025-08-04 10:36:24 +0700

    refactor: create pages folder and refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
-import Header from "./components/header/Header";
-import Body from "./components/body/Body";
+import Home from "./pages/home/Home";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Body />
+      <Home />
     </>
   );
 }
