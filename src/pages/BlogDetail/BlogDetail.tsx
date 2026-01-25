d7893bf9440e0ae072a168dba0670a54b6d20518
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2026-01-25 21:48:34 +0700

    ci: update

--- src/pages/BlogDetail/BlogDetail.tsx
+++ src/pages/BlogDetail/BlogDetail.tsx
@@ -1,45 +1,50 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router"
+import { useNavigate, useParams } from "react-router";
 import Title from "../../components/title/Title";
 import Button from "../../components/button/Button";
 import { formatDate } from "../../utils/date";
 import { TextMarkdown } from "../../components/text/Text";
 import type { Blog } from "../../types/blog";
-import styles from "./BlogDetail.module.css"
+import styles from "./BlogDetail.module.css";
 
 export function BlogDetail() {
-    const navigate = useNavigate()
-    const param = useParams();
-    const [blog, setBlog] = useState<Blog | null>();
+  const navigate = useNavigate();
+  const param = useParams();
+  const [blog, setBlog] = useState<Blog | null>();
 
-    useEffect(() => {
-        const fetchBlogDetail = async () => {
-            const response = await axios.get(`${import.meta.env.VITE_API_HOST}/public/v1/blogs/${param.id}`);
-            setBlog(response.data.data);
-        }
-        fetchBlogDetail();
-    }, [param.id])
+  useEffect(() => {
+    const fetchBlogDetail = async () => {
+      console.log(import.meta.env.VITE_API_HOST);
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_HOST}/public/v1/blogs/${param.id}`,
+      );
+      setBlog(response.data.data);
+    };
+    fetchBlogDetail();
+  }, [param.id]);
 
-    if (!blog) return null;
+  if (!blog) return null;
 
-    return (
-        <>
-            <div className="content-container">
-                <Title title={blog.title} description={`Last updated: ${formatDate(blog.updated_at)}`} />
-                <TextMarkdown>
-                    {blog.content}
-                </TextMarkdown>
-            <div className={styles.buttonContainer}>
-                <Button
-                    text="Edit"
-                    onClick={() => {navigate(`/blogs/${param.id}/edit`)}}
-                    className={styles.editButton}
-                />
-            </div>
-            <div className={styles.footer}></div>
-            </div>
-        </>
-    )
-
+  return (
+    <>
+      <div className="content-container">
+        <Title
+          title={blog.title}
+          description={`Last updated: ${formatDate(blog.updated_at)}`}
+        />
+        <TextMarkdown>{blog.content}</TextMarkdown>
+        <div className={styles.buttonContainer}>
+          <Button
+            text="Edit"
+            onClick={() => {
+              navigate(`/blogs/${param.id}/edit`);
+            }}
+            className={styles.editButton}
+          />
+        </div>
+        <div className={styles.footer}></div>
+      </div>
+    </>
+  );
 }
